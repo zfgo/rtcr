af2a205ecb569cfed7790f9b9a611ffb257511ff
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 15:25:30 -0700

    add skeleton of vec3 class

--- src/datatypes/vector.h
+++ src/datatypes/vector.h
@@ -1,10 +1,22 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
+#include <cmath>
+#include <iostream>
 
+// so we can use sqrt() instead of std::sqrt()
+using std::sqrt;
 
+/* TODO
+ */
+class vec3 {
+    public:
+        // TODO
+}
 
-
-
+/* add aliases so we can use vec3 for points and colors
+ */
+ using point3 = vec3; // 3D point (x, y, z)
+ using color = vec3; // color (r, g, b)
 
 #endif /* _VECTOR_H_ */

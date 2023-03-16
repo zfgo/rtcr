1537111c10b503a7e3a0920c524d730d72633a75
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 13:47:30 -0700

    add comments, and progress indicator

--- src/main.cc
+++ src/main.cc
@@ -11,15 +11,24 @@ int main(void)
     const int image_width = 256;
     const int image_height = 256;
 
-    /* Simple rendering */
+    /* Simple rendering loop */
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (j = image_height-1; j >= 0; --j) {
+
+        /* \r denotes a carriage return
+         * std::flush flushes the output sequence os
+         */
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
         for (i = 0; i < image_width; ++i) {
             r = (float) i / (image_width - 1);
             g = (float) j / (image_height - 1);
             b = 0.25;
 
+            /* static_cast<type> is used for when we want to reverse an
+             * implicit conversion. It performs no runtime checks.
+             */
             ir = static_cast<int>(255.9999 * r);
             ig = static_cast<int>(255.9999 * g);
             ib = static_cast<int>(255.9999 * b);
@@ -27,5 +36,7 @@ int main(void)
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::cerr << "\nDone.\n";
     return 0;
 }

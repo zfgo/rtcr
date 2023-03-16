02b41d19dc1318dd1bf23953f3f020af2f8089fe
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 16:16:28 -0700

    add fxn to write a color to a stream

--- src/datatypes/color.cc
+++ src/datatypes/color.cc
@@ -1,1 +1,11 @@
 #include "datatypes/color.h"
+
+ void write_color(std::ostream &out, color c)
+ {
+     /* we need to translate the color from a float that is 0 < x < 1
+      * to a integere that is 0 < x < 255
+      */
+    out << static_cast<int>(255.9999 * c.x()) << ' '
+        << static_cast<int>(255.9999 * c.y()) << ' '
+        << static_cast<int>(255.9999 * c.z()) << '\n';
+ }
--- src/datatypes/color.h
+++ src/datatypes/color.h
@@ -1,6 +1,11 @@
 #ifndef _COLOR_H_
 #define _COLOR_H_
 
+#include "datatypes/vector.h"
+#include <iostream>
 
+/* write a color to a stream
+ */
+ void write_color(std::ostream &out, color c);
 
 #endif

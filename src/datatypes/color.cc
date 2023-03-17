d0cdb34fdde9e70be9b1149e2782334980af5016
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 22:45:29 -0700

    update write_color to include samples

--- src/datatypes/color.cc
+++ src/datatypes/color.cc
@@ -1,11 +1,23 @@
 #include "datatypes/color.h"
 
- void write_color(std::ostream &out, color c)
+ void write_color(std::ostream &out, color c, int samples_per_pix)
  {
-     /* we need to translate the color from a float that is 0 < x < 1
-      * to a integere that is 0 < x < 255
-      */
-    out << static_cast<int>(255.9999 * c.x()) << ' '
-        << static_cast<int>(255.9999 * c.y()) << ' '
-        << static_cast<int>(255.9999 * c.z()) << '\n';
+    float r, g, b,
+          scale;
+
+    // get the color components
+    r = c.x();
+    g = c.y();
+    b = c.z();
+
+    // divide each color by the number of samples
+    scale = 1.0 / samples_per_pix;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.9999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.9999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.9999)) << '\n';
  }
--- src/datatypes/color.h
+++ src/datatypes/color.h
@@ -5,7 +5,11 @@
 #include <iostream>
 
 /* write a color to a stream
+ *
+ * We need to translate the color from a float that is 0 < x < 1
+ * to a integere that is 0 < x < 255, and then write it to the out 
+ * stream.
  */
- void write_color(std::ostream &out, color c);
+ void write_color(std::ostream &out, color c, int samples_per_pix);
 
 #endif

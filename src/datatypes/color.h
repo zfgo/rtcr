60ea9ef6f9908911bb5bdfcfe0e9db856e2d5ecd
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-17 09:22:54 -0700

    add gamma correction

--- src/datatypes/color.h
+++ src/datatypes/color.h
@@ -9,6 +9,10 @@
  * We need to translate the color from a float that is 0 < x < 1
  * to a integere that is 0 < x < 255, and then write it to the out 
  * stream.
+ * 
+ * The color will appear darker than we originally intend, because it
+ * is not ``gamma corrected''. We fix this by taking the sqrt of the
+ * color.
  */
  void write_color(std::ostream &out, color c, int samples_per_pix)
  {
@@ -20,12 +24,12 @@
     g = c.y();
     b = c.z();
 
-    // divide each color by the number of samples
+    // divide each color by the number of samples, and gamma correct
+    // for gamma = 2.0 (raise to power of 1/gamma, or sqrt)
     scale = 1.0 / samples_per_pix;
-    r *= scale;
-    g *= scale;
-    b *= scale;
-
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     out << static_cast<int>(256 * clamp(r, 0.0, 0.9999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.9999)) << ' '

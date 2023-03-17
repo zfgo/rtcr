813b081227ac45ed91492429f185e519587be241
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-17 09:56:18 -0700

    implement inherited scatter fxn

--- src/datatypes/material.h
+++ src/datatypes/material.h
@@ -10,13 +10,27 @@ class material
     public:
         /* fxn to be implemented by base classes
          */
-        virtual bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-        ) const = 0;
+        virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
 class lambertian : public material
 {
+    public:
+        /* constructor */
+        lambertian(const color& a) : albedo(a) { }
+
+
+        /* implement the scatter fxn inherited from the parent class.
+         * This function calculates a random scattering of a ray on a 
+         * lambertian surface
+         */
+        virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override 
+        {
+            vector scatter_dir = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_dir);
+            attenuation = albedo;
+            return true;
+        }
 
 };
 

65e6ad0c76c1365675d34e8a760015a303cf59c7
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 20:54:39 -0700

    add hit_record struct and hittable class

--- src/datatypes/hittable.h
+++ src/datatypes/hittable.h
@@ -1,6 +1,25 @@
 #ifndef _HITTABLE_H_
 #define _HITTABLE_H_
 
+#include "datatypes/ray.h"
+
+/* structure for each hit record (where a ray bounces off of an object.
+ * The hit point, normal at that point, and time need to be recorded.
+ */
+struct hit_record {
+    point3 p;
+    vec3 normal;
+    float t;
+};
+
+/* Because we have many rays bouncing off of many objects, we keep 
+ * track of them in a large array
+ */
+class hittable 
+{
+    public:
+        virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
 
 
 #endif /* _HITTABLE_H_ */

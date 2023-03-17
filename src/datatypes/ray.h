a0301a97835abe07a0b95394240b3ea4455ec0bb
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 17:54:58 -0700

    add constructors

--- src/datatypes/ray.h
+++ src/datatypes/ray.h
@@ -3,7 +3,17 @@
 
 #include "datatypes/vector.h"
 
+/* TODO
+ */
 class ray {
+    /* public member functions of the ray class
+     */
+    public: 
+        /* constructors for the ray class
+         */
+        ray() {}
+
+        ray(const point3& origin, const vector& direction) : origin(origin), dir(direction) {}
 
 };
 

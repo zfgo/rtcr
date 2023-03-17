e9178f83ab74788d63df7cfd53927fa385aea413
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-17 09:13:35 -0700

    remove color.cc, incorporate everything into the header

--- src/datatypes/color.h
+++ src/datatypes/color.h
@@ -10,6 +10,25 @@
  * to a integere that is 0 < x < 255, and then write it to the out 
  * stream.
  */
- void write_color(std::ostream &out, color c, int samples_per_pix);
+ void write_color(std::ostream &out, color c, int samples_per_pix)
+ {
+    float r, g, b,
+          scale;
 
+    // get the color components
+    r = c.x();
+    g = c.y();
+    b = c.z();
+
+    // divide each color by the number of samples
+    scale = 1.0 / samples_per_pix;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.9999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.9999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.9999)) << '\n';
+ }
 #endif

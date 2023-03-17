c1b22730a777d3bd19794092ab789c8e2f258081
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-17 09:46:50 -0700

    add ASB fxn to be implemented by base classes

--- src/datatypes/material.h
+++ src/datatypes/material.h
@@ -7,7 +7,12 @@ struct hit_record; // forward declaration
 
 class material
 {
-    
+    public:
+        /* fxn to be implemented by base classes
+         */
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const = 0;
 };
 
 #endif /* _MATERIAL_H_ */

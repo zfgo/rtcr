09a51ae04aafd70fdbb346a651037b357ba06686
Zane <zanegloh@gmail.com>
2023-03-17 11:57:41 -0700

    update scene

--- src/main.cc
+++ src/main.cc
@@ -59,11 +59,15 @@ int main(void)
     float R = cos(pi / 4.0);
     hittable_list world;
 
-    auto material_left = make_shared<lambertian>(color(0.0, 0.0, 1.0));
-    auto material_right = make_shared<lambertian>(color(1.0, 0.0, 0.0));
+    auto material_grnd = make_shared<lambertian>(color(0.1, 0.8, 0.2));
+    auto material_cntr = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    world.add(make_shared<sphere>(point3(-R, 0.0, -1.0),  R, material_left));
-    world.add(make_shared<sphere>(point3(R, 0.0, -1.0), R, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_grnd));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_cntr));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     /* set up Camera */
     camera cam(90.0, aspect_ratio);

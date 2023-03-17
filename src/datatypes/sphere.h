78f7c2981cc0adbb35cb7943aed7463d5445a7dd
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-03-16 20:56:24 -0700

    start implementation of sphere class

--- src/datatypes/sphere.h
+++ src/datatypes/sphere.h
@@ -1,6 +1,22 @@
 #ifndef _SPHERE_H_
 #define _SPHERE_H_
 
+#include "datatypes/hittable.h"
+#include "datatypes/vector.h"
 
+/* because a sphere can be hit by a ray, it inherits from the hittable
+ * class.
+ */
+class sphere : public hittable
+{
+    public:
+        sphere() {}
+
+
+    public:
+        point3 center;
+        float radius;
+};
+    
 
 #endif /* _SPHERE_H_ */
